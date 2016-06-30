67eb6df5b625166f237856551905a6454366d16d
Yichuan Shen <shen.yichuan@gmail.com>
2016-06-30 13:41:15 +0200

    Some basic code for Gauss-Newton

--- Extra/VGN.m
+++ Extra/VGN.m
@@ -1,4 +1,17 @@
 function [iterations, xstar] = VGN(f, J, x0, dimension)
+    iterations = 0;
+    xstar = x0;
+    r = f(:, dimension + 1);
     
+    while 1
+        xold = xstar;
+        xstar = xold - (D' * D) \ (D' * r);
+              
+        iterations = iterations + 1;
+
+        if abs(xstar - xold) < 10^(-8)
+            return
+        end
+    end
 end
 
